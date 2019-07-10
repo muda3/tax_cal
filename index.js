f0ed54330c6b3e9315f921ab46f0d1f461534713
muda3 <yunepehe@f5.si>
2019-07-10 23:47:45 +0900

    ２

---
+++ index.js
@@ -0,0 +1,23 @@
+const electron = require("electron");
+const loadDevtool = require('electron-load-devtool');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("ready", function () {
+  // ブラウザ(Chromium)の起動, 初期画面のロード
+  mainWindow = new BrowserWindow({ width: 400, height: 300 });
+  mainWindow.loadURL("file://" + __dirname + "/index.html");
+  loadDevtool(loadDevtool.REDUX_DEVTOOLS);
+  mainWindow.openDevTools();
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+});
