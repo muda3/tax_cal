dd5c63e56af335ece846dac77c5ef32a10c338ce
muda3 <yunepehe@f5.si>
2019-07-29 14:39:29 +0900

    fix code

--- cal.js
+++ cal.js
@@ -1,12 +1,10 @@
 function cal() {
-<<<<<<< cc4398bbcd6f716cd5146b66b78155ec91a6342c
+
     const zeibetu = document.getElementById("okane").value;
     const tax = document.getElementById("tax").value;
     const calc = zeibetu * tax;
     document.getElementById("kekka").value = calc;
-=======
     const zeibetsu = document.getElementById("okane").value;
     const calc = zeibetsu * tax;
     document.getElementById("kekka").value = parseInt(calc);
->>>>>>> cおｍｐ
 }
